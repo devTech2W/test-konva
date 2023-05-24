5b1d4bf5c0fd2d6e7a1deec55a09a85aef6f8bb4
dev01Tech2W <132885668+devTech2W@users.noreply.github.com>
2023-05-24 16:16:16 +0200

    add point to nearest 2 points commit

--- src/App.js
+++ src/App.js
@@ -50,11 +50,11 @@ export default function App() {
     points.length === 0 ? setConfirm(false) : null;
   }, [points, confirm]);
 
-  const handleCloseShape = () => {
-    if (points.length >= 2) {
-      setPoints([...points, points[0], points[1]]);
-    }
-  };
+  // const handleCloseShape = () => {
+  //   if (points.length > 0) {
+  //     setPoints([...points, points[0]]);
+  //   }
+  // };
 
   const handleColorChange = (newColor) => {
     setShape({ ...shape, color: newColor });
@@ -90,15 +90,11 @@ export default function App() {
     if (isDrawing) {
       const stage = e.target.getStage();
       const point = stage.getPointerPosition();
-      if (points.length > 0) {
-        const avantDernier = points[points.length - 2];
-        const dernier = points[points.length - 1];
-        points.pop();
-        points.pop();
-        const newPoints = [...points, avantDernier, dernier, point.x, point.y];
+      if (points.length >= 1) {
+        const newPoints = [...points, { x: point.x, y: point.y }];
         setPoints(newPoints);
       } else {
-        setPoints([point.x, point.y]);
+        setPoints([{ x: point.x, y: point.y }]);
       }
     } else return;
   };
@@ -106,7 +102,7 @@ export default function App() {
   const handleUndo = async () => {
     if (points.length > 0) {
       let currentPoints = [];
-      for (let i = 0; i !== points.length - 2; i++) {
+      for (let i = 0; i !== points.length - 1; i++) {
         // eslint-disable-next-line no-unused-expressions
         await currentPoints.push(points[i]);
       }
@@ -115,12 +111,16 @@ export default function App() {
     }
   };
 
+  useEffect(() => {
+    console.log(shapeList);
+  }, [shapeList]);
+
   const handleUndoEdit = async () => {
-    if (shapeToEdit?.points.length >= 2) {
-      for (let i = 0; i < 2; i++) {
-        shapeToEdit.points.pop();
-      }
-    } else if (shapeToEdit !== null && shapeToEdit?.points.length <= 4) {
+    if (shapeToEdit?.points.length > 1) {
+      const newPoints = [...shapeToEdit.points];
+      newPoints.pop();
+      setShapeToEdit({ ...shapeToEdit, points: newPoints });
+    } else if (shapeToEdit !== null && shapeToEdit?.points.length <= 1) {
       // Supprimer shapeToEdit de shapeList
       const newShapeList = shapeList.filter(
         (shape) => shape.shape_id !== shapeToEdit.shape_id
@@ -187,6 +187,7 @@ export default function App() {
     setShapeList(newShapeList);
     setIsEditing(false);
     setShapeToEdit(null);
+    setShowMessage(false);
   };
 
   const handleDragMove = (e, x, y) => {
@@ -202,8 +203,6 @@ export default function App() {
 
     newShape.points.splice(emplacementAncienX, 1, newX);
     newShape.points.splice(emplacementAncienY, 1, newY);
-
-    setShapeToEdit(newShape);
   };
 
   const handleDragEnd = (e) => {
@@ -211,6 +210,8 @@ export default function App() {
   };
 
   const handleAddPointEdit = (e) => {
+    console.log(shapeToEdit);
+    // vérifier les 2 points les plus proches
     const stage = e.target.getStage();
     const point = stage.getPointerPosition();
 
@@ -219,33 +220,66 @@ export default function App() {
 
     // Vérifier s'il y a déjà un point à proximité
     const nearbyPoint = newShape.points.some((pt, index) => {
-      if (index % 2 === 0) {
-        const x = newShape.points[index];
-        const y = newShape.points[index + 1];
-        const distance = Math.sqrt((x - point.x) ** 2 + (y - point.y) ** 2);
-        return distance < 10; // Définissez une distance de seuil appropriée
-      }
-      return false;
+      const x = newShape.points[index].x;
+      const y = newShape.points[index].y;
+      const distance = Math.sqrt((x - point.x) ** 2 + (y - point.y) ** 2);
+      return distance < 10; // Définissez une distance de seuil appropriée
     });
-
     if (!nearbyPoint) {
-      newShape.points.push(point.x);
-      newShape.points.push(point.y);
+      const { x, y } = e.target.getStage().getPointerPosition();
+      const nouvelObjet = { x, y };
+      let point1 = null;
+      let point2 = null;
+      let distanceMin1 = Number.MAX_VALUE;
+      let distanceMin2 = Number.MAX_VALUE;
+
+      shapeToEdit.points.forEach((objet, index) => {
+        const distance = Math.sqrt(
+          Math.pow(objet.x - point.x, 2) + Math.pow(objet.y - point.y, 2)
+        );
+
+        if (distance < distanceMin1) {
+          distanceMin2 = distanceMin1;
+          distanceMin1 = distance;
+          point2 = point1;
+          point1 = { ...objet, index };
+        } else if (distance < distanceMin2) {
+          distanceMin2 = distance;
+          point2 = { ...objet, index };
+        }
+      });
+
+      const nouveauTableau = [...shapeToEdit.points];
+
+      // Vérifier si les deux points les plus proches sont adjacents
+      if (Math.abs(point1.index - point2.index) === 1) {
+        // Les deux points sont adjacents, insérer le nouveau point après le premier point
+        nouveauTableau.splice(point1.index + 1, 0, nouvelObjet);
+      } else {
+        // Les deux points ne sont pas adjacents, insérer le nouveau point entre les deux points
+        const insertionIndex = Math.max(point1.index, point2.index);
+        nouveauTableau.splice(insertionIndex, 0, nouvelObjet);
+      }
+
+      const newShape = { ...shapeToEdit, points: nouveauTableau };
+      setShapeToEdit(newShape);
+      const newList = [...shapeList];
+      // Mettre à jour la liste des formes avec la forme modifiée
+      const replace = shapeList.indexOf(shapeToEdit);
+      newList.splice(replace, 1, newShape);
+      setShapeList(newList);
     }
 
-    // Mettre à jour la liste des formes avec la forme modifiée
-    const replace = shapeList.indexOf(shapeToEdit);
-    newList.splice(replace, 1, newShape);
-    setShapeList(newList);
     setShape({});
-    setIsDrawing(false);
-    setPoints([]);
-    setConfirm(false);
   };
 
   useEffect(() => {
     setShapeToEdit(shapeToEdit);
   }, [shapeToEdit]);
+
+  useEffect(() => {
+    setShapeList(shapeList);
+  }, [shapeList]);
 
   const handleFinishEdit = () => {
     if (shapeToEdit !== null && shapeToEdit?.points.length <= 4) {
@@ -260,9 +294,6 @@ export default function App() {
     }
 
     setShapeToEdit(null);
-    setNewPoints(null);
-    setPointsToReplace(null);
-    setIsEditing(false);
   };
 
   return (
@@ -295,11 +326,13 @@ export default function App() {
         width={50}
         height={50}
       >
-        {isDrawing
+        {isDrawing && points.length > 0
           ? "Terminer"
           : isEditing
           ? "Modifications terminées"
-          : "Créer une zone"}
+          : !isDrawing
+          ? "Créer une zone"
+          : null}
       </button>
       {points.length > 0 ||
       (shapeToEdit && shapeToEdit.points.length > 0 && isEditing) ? (
@@ -350,12 +383,6 @@ export default function App() {
       >
         <Layer style={{ padding: "2em" }}>
           <Image ref={planRef} />
-          <Rect
-            style={{ padding: "2em" }}
-            width={1000}
-            height={1000}
-            fill="#00000080"
-          />
 
           {shapeList.length > 0
             ? shapeList?.map((shape) => {
@@ -365,8 +392,8 @@ export default function App() {
                     <Text
                       fill={"black"}
                       text={shape?.name}
-                      x={pts ? pts[0] + 5 : null}
-                      y={pts ? pts[1] - 35 : null}
+                      x={pts ? pts[0].x + 5 : null}
+                      y={pts ? pts[0].y - 35 : null}
                       fontSize={20}
                       fontFamily="Arial"
                       padding={10}
@@ -377,17 +404,15 @@ export default function App() {
                     <Shape
                       sceneFunc={(context, shape) => {
                         context.beginPath();
-                        context.moveTo(pts[0], pts[1]);
+                        context.moveTo(pts[0].x, pts[0].y);
                         pts.forEach((point, i) => {
-                          if (i % 2 === 0 && i > 1) {
-                            context.lineTo(pts[i], pts[i + 1]);
-                          }
+                          context.lineTo(pts[i].x, pts[i].y);
                         });
                         context.closePath();
                         context.fillStrokeShape(shape);
                       }}
                       fill={`${shape.color}90`}
-                      onClick={handleCloseShape}
+                      // onClick={handleCloseShape}
                       onDblClick={() => {
                         setShowMessage(true);
                         setShapeToEdit(shape);
@@ -395,34 +420,32 @@ export default function App() {
                     />
                     {isEditing && shape === shapeToEdit && pts.length > 0
                       ? pts.map((pt, index) => {
-                          if (index % 2 === 0) {
-                            const x = pts[index];
-                            const y = pts[index + 1];
-                            return (
-                              <Circle
-                                onDragStart={() => {
-                                  setPointsToReplace({
-                                    x: x,
-                                    y: y,
-                                  });
-                                }}
-                                onDragMove={(e) => {
-                                  isEditing && pointsToReplace !== null
-                                    ? handleDragMove(e, x, y)
-                                    : null;
-                                }}
-                                onDragEnd={(e) => {
-                                  handleDragEnd(e);
-                                }}
-                                key={`circle_${index}`}
-                                draggable
-                                x={x}
-                                y={y}
-                                radius={5}
-                                fill="yellow"
-                              />
-                            );
-                          }
+                          const x = pts[index].x;
+                          const y = pts[index].y;
+                          return (
+                            <Circle
+                              onDragStart={() => {
+                                setPointsToReplace({
+                                  x: x,
+                                  y: y,
+                                });
+                              }}
+                              onDragMove={(e) => {
+                                isEditing && pointsToReplace !== null
+                                  ? handleDragMove(e, x, y)
+                                  : null;
+                              }}
+                              onDragEnd={(e) => {
+                                handleDragEnd(e);
+                              }}
+                              key={`circle_${index}`}
+                              draggable
+                              x={x}
+                              y={y}
+                              radius={5}
+                              fill="yellow"
+                            />
+                          );
                         })
                       : null}
                   </Group>
@@ -433,52 +456,44 @@ export default function App() {
             <Shape
               sceneFunc={(context, shape) => {
                 context.beginPath();
-                context.moveTo(points[0], points[1]);
+                context.moveTo(points[0].x, points[0].y);
                 points.forEach((point, i) => {
-                  if (i % 2 === 0 && i > 1) {
-                    context.lineTo(points[i], points[i + 1]);
-                  }
+                  context.lineTo(points[i].x, points[i].y);
                 });
                 context.closePath();
                 context.fillStrokeShape(shape);
               }}
               fill={`${shape.color}80`}
-              onClick={handleCloseShape}
+              // onClick={handleCloseShape}
             />
           ) : null}
-          {points.map((point, index) => {
-            if (index % 2 === 0) {
-              const x = points[index];
-              const y = points[index + 1];
-              return (
-                <Circle
-                  key={`circle_${index}`}
-                  x={x}
-                  y={y}
-                  radius={5}
-                  fill="yellow"
-                />
-              );
-            }
-            return null;
+          {points?.map((point, index) => {
+            const x = points[index].x;
+            const y = points[index].y;
+            return (
+              <Circle
+                key={`circle_${index}`}
+                x={x}
+                y={y}
+                radius={5}
+                fill="yellow"
+              />
+            );
           })}
           {shape === shapeToEdit
             ? pts?.map((point, index) => {
-                if (index % 2 === 0) {
-                  const x = pts[index];
-                  const y = pts[index + 1];
-                  return (
-                    <Circle
-                      draggable={shapeToEdit === shape}
-                      key={`circle_${index}`}
-                      x={x}
-                      y={y}
-                      radius={5}
-                      fill="yellow"
-                    />
-                  );
-                }
-                return null;
+                const x = pts[index].x;
+                const y = pts[index].y;
+                return (
+                  <Circle
+                    draggable={shapeToEdit === shape}
+                    key={`circle_${index}`}
+                    x={x}
+                    y={y}
+                    radius={5}
+                    fill="yellow"
+                  />
+                );
               })
             : null}
         </Layer>
